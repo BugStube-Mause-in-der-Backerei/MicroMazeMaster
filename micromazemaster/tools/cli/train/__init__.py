5c2a6624deacf0101e6f7d2cd1ce08704ca769cc
Benjamin Langer <info@benjaminlanger.dev>
2024-12-18 17:03:55 +0100

    Docker build github actions workflow

---
+++ micromazemaster/tools/cli/train/__init__.py
@@ -0,0 +1,84 @@
+import random
+
+import typer
+from typing_extensions import Annotated
+
+train_cli = typer.Typer(no_args_is_help=True)
+
+
+@train_cli.callback()
+def train_callback():
+    """Micromazemaster module for training"""
+    pass
+
+
+@train_cli.command("evolution")
+def cmd_train_evolution(
+    num_generations: Annotated[int, typer.Option(help="Number of generations to train.")] = 2500,
+    population_size: Annotated[int, typer.Option(help="Size of the population.")] = 25,
+    mutation_rate: Annotated[float, typer.Option(help="The rate at which mutation occurs.")] = 0.1,
+    crossover_rate: Annotated[float, typer.Option(help="The rate at which crossover occurs.")] = 0.5,
+    starting_steps: Annotated[int, typer.Option(help="Number of steps to start with.")] = 10,
+    parallel_execution: Annotated[bool, typer.Option(help="Whether to use parallel execution.")] = True,
+    seed: Annotated[int, typer.Option(help="Seed for reproducable results.", show_default="random")] = random.randint(
+        0, 100000
+    ),
+):
+    """Train a model using the evolutionary algorithm"""
+    import atexit
+    import os
+    import random
+    from datetime import datetime
+    from pathlib import Path
+
+    import torch
+
+    from micromazemaster.models.maze import Maze
+    from micromazemaster.utils.evolution import Evolution
+    from micromazemaster.utils.logging import logger
+
+    timestamp = datetime.now().strftime("%Y-%m-%dT%H-%M-%S.%f")
+    CWD = Path(os.getcwd())
+    WORKING_DIR = CWD.joinpath("local_data", timestamp)
+    try:
+        WORKING_DIR.mkdir(parents=True)
+        logger.debug(f"Working directory created: {WORKING_DIR}")
+
+    except OSError as e:
+        logger.error(f"Failed to create working directory: {e}")
+        exit(1)
+
+    def exit_handler():
+        logger.info("Saving best model...")
+        try:
+            evo.save_as_state_dict(WORKING_DIR.joinpath("best_model.pth"))
+            logger.info("Best model saved.")
+        except Exception as e:
+            logger.error(f"Failed to save best model: {e}")
+        logger.info("Exiting...")
+
+    atexit.register(exit_handler)
+
+    random.seed(seed)
+
+    torch.manual_seed(seed)
+
+    seeds = [random.randint(0, 100000) for _ in range(31)]
+
+    mazes = [Maze(width=5, height=5, seed=seed) for seed in seeds]
+
+    train_mazes = mazes[:-1]
+    eval_maze = mazes[-1]
+
+    device = torch.device("cpu")
+    evo = Evolution(
+        num_generations=num_generations,
+        population_size=population_size,
+        mutation_rate=mutation_rate,
+        crossover_rate=crossover_rate,
+        device=device,
+    )
+    best_model = evo.train(
+        train_mazes=train_mazes, starting_steps=starting_steps, parallel_execution=parallel_execution
+    )
+    logger.debug(f"Best model: {best_model.state_dict()}")
